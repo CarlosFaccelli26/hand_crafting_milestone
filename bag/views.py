37ba03919e749eab2868d47c04257caafd27a506
CarlosFaccelli26 <carlosfaccelli@gmail.com>
2021-06-12 09:39:23 +0000

    Added remove-link funcionality and jquery link on base.html

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 
 # Create your views here.
 
@@ -35,6 +35,21 @@ def adjust_bag(request, item_id):
         bag[item_id] = quantity
     else:
         bag.pop(item_id)
-    
+
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    try:
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
