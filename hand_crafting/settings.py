c6a343af740e061ce57bee20971de06c1bcfef5d
CarlosFaccelli26 <carlosfaccelli@gmail.com>
2021-06-14 16:17:39 +0000

    Added basic checkout funcionality

--- hand_crafting/settings.py
+++ hand_crafting/settings.py
@@ -15,7 +15,6 @@ import os
 if os.path.exists('env.py'):
     import env
 
-STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -174,6 +173,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
