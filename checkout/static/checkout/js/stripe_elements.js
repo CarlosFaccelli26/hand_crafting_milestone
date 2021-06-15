1da28a4a923fc8390ee7390a39c57e110aac99ec
CarlosFaccelli26 <carlosfaccelli@gmail.com>
2021-06-15 17:02:57 +0000

    Addded loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -48,34 +48,35 @@ card.addEventListener('change', function(event) {
 // Handle form submit
 var form = document.getElementById('payment-form');
 
-form.addEventListener('sumbit', function(ev) {
+form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({
         'disabled': true
     });
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
         }
     }).then(function(result) {
         if (result.error) {
-            // Show error to your coustumer (e.g insufficient funds)
             var errorDiv = document.getElementById('card-errors');
             var html = `
-            <span class="icon">
+                <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
-            </span>
-            <span>${result.error.message}</span>
-        `;
+                </span>
+                <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
             card.update({
                 'disabled': false
             });
             $('#submit-button').attr('disabled', false);
         } else {
-            // The payment has been processed
-            if (result.paymentIntent.status === 'succeded') {
+            if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
             }
         }
