15b1045cf0d65de33e2a49a2a82cfeb44bbd9725
CarlosFaccelli26 <carlosfaccelli@gmail.com>
2021-06-21 15:17:28 +0000

    Fixed problem sending mail with django

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -66,13 +66,13 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.phone_number =shipping_details.phone,
-                profile.country =shipping_details.address.country,
-                profile.postcode =shipping_details.address.postal_code,
-                profile.town_or_city =shipping_details.address.city,
-                profile.street_address1 =shipping_details.address.line1,
-                profile.street_address2 =shipping_details.address.line2,
-                profile.county =shipping_details.address.state,
+                profile.phone_number = shipping_details.phone,
+                profile.country = shipping_details.address.country,
+                profile.postcode = shipping_details.address.postal_code,
+                profile.town_or_city = shipping_details.address.city,
+                profile.street_address1 = shipping_details.address.line1,
+                profile.street_address2 = shipping_details.address.line2,
+                profile.county = shipping_details.address.state,
                 profile.save()
 
         order_exists = False
