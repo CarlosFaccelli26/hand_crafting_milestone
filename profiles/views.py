530e1e631608246cd29059652531328368b22889
CarlosFaccelli26 <carlosfaccelli@gmail.com>
2021-06-21 16:01:29 +0000

    Added product add functionality

--- profiles/views.py
+++ profiles/views.py
@@ -14,9 +14,12 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated.')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
