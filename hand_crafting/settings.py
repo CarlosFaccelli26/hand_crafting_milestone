a2dfe3aae9f9005f1cf86cdd760ce462846e9859
CarlosFaccelli26 <carlosfaccelli@gmail.com>
2021-07-03 12:08:36 +0000

    Removed Home app from handcrafting milestone

--- hand_crafting/settings.py
+++ hand_crafting/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'home',
     'products',
     'bag',
     'checkout',
--- hand_crafting/urls.py
+++ hand_crafting/urls.py
@@ -21,8 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path('products/', include('products.urls')),
+    path('', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
